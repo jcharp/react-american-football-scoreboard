f1cb7f7fd814bac3bfb4f8fd07b7f46b2214f4a2
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-21 22:10:21 -0400

    just getting started

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+
+const App = () + {
+
+    const [value, setValue] = useState();
+}
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
