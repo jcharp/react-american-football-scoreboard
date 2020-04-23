e84d83e90ea14302705bf40496c047af115c1484
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-22 21:35:04 -0400

    did some backtracking

--- src/index.js
+++ src/index.js
@@ -9,33 +9,69 @@ import './index.css';
     let [count, setCount] = useState(0);
 
 return (
-    <div className = "counter">
-        <h1>Team 1</h1>
-        <p className ='click_desc'>
-            This is the currnt score <span>{count}</span>
-        </p>
-        <div className="button_container">
-            <button
-            onClick = {() => {
-                setCount(count +3);
+    <div>
+           <div className = "counter">
+            <h1>Lions</h1>
+            <p className ='click_desc'>
+                This is the currnt score <span>{count}</span>
+            </p>
+            <div className="button_container">
+                <button
+                onClick = {() => {
+                    setCount(count +3);
 
-        }}
-        >
-
-        field goal
-        </button>
-        <button
-            onClick = { () => {
-                setCount(count + 7)
-
-            } }
+            }}
             >
-                Touch Down
+
+            field goal
             </button>
+            <button
+                onClick = { () => {
+                    setCount(count + 7)
+
+                } }
+                >
+                    Touch Down
+                </button>
+            </div>
+
+
         </div>
+  
+        <div className = "counter">
+            <h1>Tigers</h1>
+            <p className ='click_desc'>
+                This is the currnt score <span>{count}</span>
+            </p>
+            <div className="button_container">
+                <button
+                onClick = {() => {
+                    setCount(count +3);
+
+            }}
+            >
+
+            field goal
+            </button>
+            <button
+                onClick = { () => {
+                    setCount(count + 7)
+
+                } }
+                >
+                    Touch Down
+                </button>
+            </div>
+
+
+        </div>
+            
+
 
 
     </div>
+       
+    
 )
 
 }
