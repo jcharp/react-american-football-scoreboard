439db4025899ea0c16a7aa371c2c2fce86cb48e2
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-06-17 19:17:23 -0400

    established state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+  const homeTeam = 0;
+  const [hScore, setHomeScore] = useState(homeTeam);
+
+  const awayTeam = 0;
+  const [aScore, setAwayScore] = useState(awayTeam);
+  
+
+
   return (
     <div className="container">
       <section className="scoreboard">
